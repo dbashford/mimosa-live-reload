777992fdd17e58e0d6d301475aa4532b2823ee3e
Alxandr <alxandr@alxandr.me>
2015-01-04 00:04:00 +0100

    Fixed bug in finding path to sockets.io

--- src/assets/reload-client.js
+++ src/assets/reload-client.js
@@ -1,7 +1,7 @@
 (function() {
   var scripts = document.getElementsByTagName("script");
   var socket = io.connect(
-    (function() { for(var i=0;i<scripts.length;i++) { if(scripts[i].src && scripts[i].src.indexOf("/socket.io.js"))
+    (function() { for(var i=0;i<scripts.length;i++) { if(scripts[i].src && scripts[i].src.indexOf("/socket.io.js") > -1)
       return (/^([^#]*?:\/\/.*?)(\/.*)$/.exec(scripts[i].src) || [])[1];
     } })()
   );
