7f524e353d1fddce8cf999dc6e020e08a712fb51
david.bashford <dbashford@bericotechnologies.com>
2015-01-22 16:07:54 -0500

    slowing reload down a little bit so stylesheets can be loaded

--- src/assets/reload-client.js
+++ src/assets/reload-client.js
@@ -1,7 +1,7 @@
 (function() {
   var scripts = document.getElementsByTagName("script");
   var socket = io.connect(
-    (function() { for(var i=0;i<scripts.length;i++) { if(scripts[i].src && scripts[i].src.indexOf("/socket.io.js") > -1)
+      (function() { for(var i=0;i<scripts.length;i++) { if(scripts[i].src && scripts[i].src.indexOf("/socket.io.js") > -1)
       return (/^([^#]*?:\/\/.*?)(\/.*)$/.exec(scripts[i].src) || [])[1];
     } })()
   );
@@ -16,20 +16,23 @@
   function reloadCss() {
     var links = document.getElementsByTagName("link");
     for (var i = 0; i < links.length; i++) {
-      var original = links[i];
-      if (original.rel.toLowerCase().indexOf("stylesheet") >= 0 && original.href) {
-        var newHref = original.href.replace(/(&|%5C?)\d+/, "");
-        original.href = newHref + (newHref.indexOf("?") >= 0 ? "&" : "?") + (new Date().valueOf());
-
-        // Clone the link node and swap it out for the original
-        var clone = original.cloneNode();
-        var parent = original.parentNode;
-        var neighbor = original.nextSibling;
-        parent.insertBefore(clone, neighbor);
-        parent.removeChild(original);
+      var tag = links[i];
+      if (tag.rel.toLowerCase().indexOf("stylesheet") >= 0 && tag.href) {
+        var newHref = tag.href.replace(/(&|%5C?)\d+/, "");
+        tag.href = newHref + (newHref.indexOf("?") >= 0 ? "&" : "?") + (new Date().valueOf());
       }
     }
-    console.log('CSS updated');
+
+    // set timeout because occasionally the styles aren't
+    // ready yet and you get blank page
+    setTimeout(function() {
+      var el = document.body;
+      var bodyDisplay = el.style.display || 'block';
+      el.style.display = 'none';
+      el.offsetHeight;
+      el.style.display = bodyDisplay;
+      console.log('CSS updated');
+    }, 150);
   }
 
 })();
